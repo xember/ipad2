9b043da1e2e93417ccad7b3982c382656cd979f8
xember <peter@xember.nl>
2014-03-21 15:55:38 +0100

    barchart
    added barcharts

--- res/widgets/d3singleBar.js
+++ res/widgets/d3singleBar.js
@@ -35,17 +35,21 @@ function d3singleBar(selector, custom) {
         _data = 0,
         _svg,
         _value,
+        _bgrect,
         _rect;
     // default configuration values for this specific chart
     var config = {
+        data: [40, 50, 30, 55, 20, 50, 30],
         width: 200, // default width
         height: 200, // default height
         duration: 750, // transition duration
-        bgWidth: 30, // width of background ring
-        bgColor: "black", // bar background
-        barWidth: 25, // width of arc that represents the value
+        bgWidth: 30, // width of background bar
+        bgColor: "black", // bar backgroundcolor
+        bgbarWidth: 25, // width of background bar
         barColor: "red", // arc foreground colors (depend on threshold values)
-        barRounding: 5
+        barRounding: 5,
+        barWidth: 25,
+        spacer: 5
     }
     // overwrite config properties with custom values (if present)
     if (custom) {
@@ -64,14 +68,55 @@ function d3singleBar(selector, custom) {
         .attr("viewBox", "0 0 " + config.width + " " + config.height);
 
     //add rect background
-    _rect = _svg.selectAll("svg")
+    _bgrect = _svg.selectAll("rect")
+        .data(config.data)
+        .enter()
         .append("rect")
-        .attr("x", 10)
-        .attr("y", 10)
+        .attr("y", 0)
+        .attr("x", function(d, i) {
+            return (config.bgbarWidth + config.spacer) * i;
+        })
+        .attr("rx", config.barRounding)
+        .attr("ry", config.barRounding)
+        .attr("width", config.bgbarWidth)
+        .attr("height", config.height)
+        .style("fill", config.bgColor);
+
+    //add rect foreground
+    _rect = _svg.selectAll("svg")
+        .data(config.data)
+        .enter()
+        .append("rect")
+        .attr("x", function(d, i) {
+            return (config.bgbarWidth + config.spacer) * i;
+        })
         .attr("rx", config.barRounding)
         .attr("ry", config.barRounding)
         .attr("width", config.barWidth)
-        .attr("height", config.height);
+        .attr("height", config.height / 2)
+        .style("fill", config.barColor)
+        .attr("y", function(d) {
+            return config.height - d;
+        })
+        .attr("height", function(d) {
+            return d;
+        });
+
+    _chart.render = function(data) {
+        _rect
+            .data(data)
+            .transition()
+            .duration(config.duration)
+            .attr("y", function(d) {
+                return config.height - d;
+            })
+            .attr("height", function(d) {
+                return d;
+            });
+
+    };
+
+
 
 
     // always return chart object
